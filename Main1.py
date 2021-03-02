921c130c62283f25d7d0942bfd247c3103852b80
Nikita Gordynskii <45073693+Nikita23072001@users.noreply.github.com>
2021-03-02 17:10:30 +0100

    Angular something

--- Main1.py
+++ Main1.py
@@ -1,6 +1,5 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-print("Hello world")
 html = urlopen('https://www.ceneo.pl/')
 bs = BeautifulSoup(html.read(), 'html.parser')
 naglowki = bs.find_all('div')
