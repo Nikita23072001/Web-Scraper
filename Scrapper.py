84b0ae0b8389c5299a8f93bf8fb27b46a14a69ea
Nikita Gordynskii <45073693+Nikita23072001@users.noreply.github.com>
2021-03-06 00:15:19 +0100

    Working on Scrapper File

--- Scrapper.py
+++ Scrapper.py
@@ -5,24 +5,36 @@ import json
 html = urlopen('https://www.ceneo.pl/96961305/opinie-1')
 bs = BeautifulSoup(html.read(), 'html.parser')
 
-class scrapper():
-    id = bs.find_all('div', class_='user-post')
-
-    def scrap(self)
-    # naglowki = bs.find_all('h1')
-    # author = bs.find_all(class_='user-post__author-name')
-    # recomend = bs.find_all(class_='user-post__author-recomendation')
-    # score = bs.find_all(class_='user-post__score-count')
-    # time = bs.find_all(class_='user-post__published')
-    # text = bs.find_all(class_='user-post__text')
-    # pluses = bs.find_all(class_='review-feature')
-    # usefulness = bs.find_all(class_='js_product-review-usefulness vote')
-    # bought = bs.find_all('em')
-
+# class scrapper():
+    # def __init__(self):
+id = bs.find_all('div', class_='user-post')
+naglowki = bs.find('h1').get_text()
 file = open('scrapped.json', 'a', encoding='utf-8')
-array = []
 
-
+    # def scrap(self):
+array =[]
+for i in range(1):
+    author = id[i].find(class_='user-post__author-name').get_text()
+    recomend = id[i].find(class_='user-post__author-recomendation').get_text()
+    score = id[i].find(class_='user-post__score-count').get_text()
+    time = id[i].find(class_='user-post__published').get_text()
+    text = id[i].find(class_='user-post__text').get_text()
+    if id[i].find(class_='review-feature') == None:
+        pluses = 0
+    else:
+        pluses = id[i].find(class_='review-feature').get_text()
+    usefulness = id[i].find(class_='js_product-review-usefulness vote').get_text()
+    bought = id[i].find('em').get_text()
+    array.append(author)
+    array.append(recomend)
+    array.append(score)
+    array.append(time)
+    array.append(text)
+    array.append(pluses)
+    array.append(usefulness)
+    array.append(bought)
+    file.write(str(array))
+    file.close()
 
 def chomps(s):
     return s.rstrip('\n')
